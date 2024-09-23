1d9069043635a09bc3f343aaafa4f99e4b0722d9
zlin19-1961430 <annnzt@outlook.com>
2024-09-23 03:36:40 -0700

    Upload Zip

---
+++ Build/Lumina_Data/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,121 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID BUTTON = 977454165U;
+        static const AkUniqueID CHURCHSTATE = 922071743U;
+        static const AkUniqueID DUCK = 2617048678U;
+        static const AkUniqueID FT = 1802970383U;
+        static const AkUniqueID FT_CONCRETE = 737291327U;
+        static const AkUniqueID FT_GRASS = 904917388U;
+        static const AkUniqueID GRAVEYARDSTATE = 2709905299U;
+        static const AkUniqueID HOSPITALSTATE = 630549594U;
+        static const AkUniqueID JUMP = 3833651337U;
+        static const AkUniqueID LOSE_VO = 4294695564U;
+        static const AkUniqueID LOSECONTROL = 2423784589U;
+        static const AkUniqueID LOSEMUSIC = 2679087499U;
+        static const AkUniqueID MENUSTATE = 1548586727U;
+        static const AkUniqueID MINDLESS = 4031343534U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID PAUSE = 3092587493U;
+        static const AkUniqueID RESUME = 953277036U;
+        static const AkUniqueID STOPALL = 3086540886U;
+        static const AkUniqueID STOPMUSIC = 1917263390U;
+        static const AkUniqueID WIN_VO_FINAL = 224968472U;
+        static const AkUniqueID WIN_VO_MIDDLE = 1253664871U;
+        static const AkUniqueID WINMUSIC = 3870470286U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace GAMESTATES
+        {
+            static const AkUniqueID GROUP = 777429653U;
+
+            namespace STATE
+            {
+                static const AkUniqueID CHURCH = 2568407992U;
+                static const AkUniqueID GRAVEYARD = 992774748U;
+                static const AkUniqueID HOSPITAL = 1640084191U;
+                static const AkUniqueID LOSE = 221232726U;
+                static const AkUniqueID LOSECONTROL = 2423784589U;
+                static const AkUniqueID MENU = 2607556080U;
+                static const AkUniqueID MINDLESS = 4031343534U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID STOPMUSIC = 1917263390U;
+                static const AkUniqueID WIN = 979765101U;
+            } // namespace STATE
+        } // namespace GAMESTATES
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace FOOTSTEP
+        {
+            static const AkUniqueID GROUP = 1866025847U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID CONCRETE = 841620460U;
+                static const AkUniqueID GRASS = 4248645337U;
+            } // namespace SWITCH
+        } // namespace FOOTSTEP
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID GAMPLAYSIDECHAIN = 575675012U;
+        static const AkUniqueID SOUNDSIDECHAIN = 1492793234U;
+        static const AkUniqueID SS_AIR_FEAR = 1351367891U;
+        static const AkUniqueID SS_AIR_FREEFALL = 3002758120U;
+        static const AkUniqueID SS_AIR_FURY = 1029930033U;
+        static const AkUniqueID SS_AIR_MONTH = 2648548617U;
+        static const AkUniqueID SS_AIR_PRESENCE = 3847924954U;
+        static const AkUniqueID SS_AIR_RPM = 822163944U;
+        static const AkUniqueID SS_AIR_SIZE = 3074696722U;
+        static const AkUniqueID SS_AIR_STORM = 3715662592U;
+        static const AkUniqueID SS_AIR_TIMEOFDAY = 3203397129U;
+        static const AkUniqueID SS_AIR_TURBULENCE = 4160247818U;
+    } // namespace GAME_PARAMETERS
+
+    namespace TRIGGERS
+    {
+        static const AkUniqueID MUSICTRANSITION = 4186865675U;
+    } // namespace TRIGGERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID OTHERMUSIC = 223058466U;
+        static const AkUniqueID PLAYERSOUND = 2656385039U;
+        static const AkUniqueID THEMEMUSIC = 450773281U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
